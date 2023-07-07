61679fd5eb40ef15b827757874d8e18784183ece
quinise <quinise.ercolano@gmail.com>
2023-07-07 09:36:47 -0700

    updated styles - font sizes larger

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,9 +6,16 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RulesComponent } from './components/rules/rules.component';
 import { StructureComponent } from './components/structure/structure.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { OfficialsComponent } from './components/officials/officials.component';
 import { ErrorComponent } from './components/error/error.component';
 import { HomeComponent } from './components/home/home.component';
 import { LayoutModule } from '@angular/cdk/layout';
+
+import { StructureService } from './services/structure.service';
+import { OfficialsService } from './services/officials.service';
+import { RulesService } from './services/rules.service';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatMenuModule } from '@angular/material/menu'
@@ -17,14 +24,6 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { OfficialsComponent } from './components/officials/officials.component';
-import { StructureService } from './services/structure.service';
 
 @NgModule({
   declarations: [
@@ -49,13 +48,9 @@ import { StructureService } from './services/structure.service';
     MatIconModule,
     MatListModule,
     BrowserAnimationsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    FormsModule,
-    ReactiveFormsModule,
     CommonModule
   ],
-  providers: [StructureService],
+  providers: [StructureService, OfficialsService, RulesService],
   bootstrap: [AppComponent]
 })
 
